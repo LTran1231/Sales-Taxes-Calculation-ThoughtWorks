d9b8fa95c0a06325e79d242b22ab477c6a9c6039
Le Tran <LTran.1231@gmail.com>
2015-10-13 18:48:09 -0700

    complete test for shopping basket 3

--- test/PurchaseReceiptTest.java
+++ test/PurchaseReceiptTest.java
@@ -11,7 +11,8 @@ import static org.hamcrest.core.Is.is;
 public class PurchaseReceiptTest {
     ShoppingBasket shoppingBasket_1;
     ShoppingBasket shoppingBasket_2;
-    
+    ShoppingBasket shoppingBasket_3;
+
 
 
     @Before
@@ -47,12 +48,12 @@ public class PurchaseReceiptTest {
     }
 
     @Test
-    public void testShoppingCartIsEmpty() {
+    public void testShoppingBasket2IsEmpty() {
         assertThat(shoppingBasket_2.getShoppingItems(), is(IsNull.notNullValue()));
     }
     
     @Test
-    public void testShoppingBasket_2(){
+    public void testShoppingBasket2(){
         Item box_of_chocolates = new Item("imported box of chocolates", "food", "imported", new BigDecimal("1"), new BigDecimal("10.00"));
         Item bottle_of_perfume = new Item("imported bottle of perfume", "perfume", "imported", new BigDecimal("1"), new BigDecimal("47.50"));
 
@@ -63,6 +64,37 @@ public class PurchaseReceiptTest {
 
         assertThat(shoppingBasket_2.getSalesTax(), is(new BigDecimal("7.65")));
         assertThat(shoppingBasket_2.getTotal(), is(new BigDecimal("65.15")));
+    }
+    
+    @Before
+    public void setUpShoppingBasket3() {
+        shoppingBasket_3 = new ShoppingBasket();
+    }
+
+    @Test
+    public void testShoppingBasket3IsEmpty() {
+        assertThat(shoppingBasket_3.getShoppingItems(), is(IsNull.notNullValue()));
+    }
+
+    @Test
+    public void testShoppingBasket3() {
+        Item bottle_of_perfume_1 = new Item("imported bottle of perfume", "perfume", "imported", new BigDecimal("1"), new BigDecimal("27.99"));
+        Item bottle_of_perfume_2 = new Item("bottle of perfume", "perfume", "domestic", new BigDecimal("1"), new BigDecimal("18.99"));
+        Item packet_of_headache_pills = new Item("packet of headache pills", "medical", "domestic", new BigDecimal("1"), new BigDecimal("9.75"));
+        Item box_of_imported_chocolates = new Item("box of imported chocolates", "food", "imported", new BigDecimal("1"), new BigDecimal("11.25"));
+
+        shoppingBasket_3.addShoppingItem(bottle_of_perfume_1);
+        assertThat(shoppingBasket_3.getTotal(), is(new BigDecimal("32.19")));
+        shoppingBasket_3.addShoppingItem(bottle_of_perfume_2);
+        assertThat(shoppingBasket_3.getTotal().subtract(new BigDecimal("32.19")), is(new BigDecimal("20.89")));
+        shoppingBasket_3.addShoppingItem(packet_of_headache_pills);
+        assertThat(shoppingBasket_3.getTotal().subtract(new BigDecimal("32.19")).subtract(new BigDecimal("20.89")), is(new BigDecimal("9.75")));
+        shoppingBasket_3.addShoppingItem(box_of_imported_chocolates);
+        assertThat(shoppingBasket_3.getTotal().subtract(new BigDecimal("32.19")).subtract(new BigDecimal("20.89")).subtract(new BigDecimal("9.75")), is(new BigDecimal("11.85")));
+
+
+        assertThat(shoppingBasket_3.getSalesTax(), is(new BigDecimal("6.70")));
+        assertThat(shoppingBasket_3.getTotal(), is(new BigDecimal("74.68")));
     }
     
 
