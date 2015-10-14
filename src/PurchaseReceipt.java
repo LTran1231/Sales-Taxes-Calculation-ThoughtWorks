c26dad56cd33114add6bbf4f78ae8bacdb0de39a
Le Tran <LTran.1231@gmail.com>
2015-10-13 19:14:41 -0700

    prettify receipt

--- src/PurchaseReceipt.java
+++ src/PurchaseReceipt.java
@@ -3,7 +3,7 @@ import java.math.BigDecimal;
 public class PurchaseReceipt {
     static BigDecimal appleCost = new BigDecimal("1.90");
     public static void main(String[] args) {
-        System.out.println("Customer Shopping Receipt 1");
+        System.out.println("CUSTOMER SHOPPING RECEIPT 1");
         ShoppingBasket shoppingBasket_1 = new ShoppingBasket();
         Item book = new Item("book", "book", "domestic", new BigDecimal("1"), new BigDecimal("12.49")); 
         Item music = new Item("music CD", "music", "domestic", new BigDecimal("1"), new BigDecimal("14.99"));
@@ -12,14 +12,21 @@ public class PurchaseReceipt {
         shoppingBasket_1.addShoppingItem(book);
         shoppingBasket_1.addShoppingItem(music);
         shoppingBasket_1.addShoppingItem(chocolate_bar);
-        
-        
+        System.out.println("===============================================================================");
+        System.out.println("Qtn"+ " Name" + "                               Price");
+        System.out.println("===============================================================================");
+        System.out.println(book.getQtn() + "   " + book.getName() + "                               $" + book.getPrice());
+        System.out.println(music.getQtn() + "   " + music.getName() + "                           $" + music.getPrice());
+        System.out.println(chocolate_bar.getQtn() + "   " + chocolate_bar.getName()+ "                      $" + chocolate_bar.getPrice());
+        System.out.println("----------------------------------------------------");
         System.out.println("Subtotal: $" + shoppingBasket_1.getSubTotal());
         System.out.println("Sales tax: $" + shoppingBasket_1.getSalesTax());
         System.out.println("Total: $" + shoppingBasket_1.getTotal());
-        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("===============================================================================");
+
 // ------------------------------------------------------------------------- 
-        System.out.println("Customer Shopping Receipt 2");
+        System.out.println();
+        System.out.println("CUSTOMER SHOPPING RECEIPT 2");
         ShoppingBasket shoppingBasket_2 = new ShoppingBasket();
         
         Item box_of_chocolates = new Item("imported box of chocolates", "food", "imported", new BigDecimal("1"), new BigDecimal("10.00"));
@@ -28,12 +35,19 @@ public class PurchaseReceipt {
         shoppingBasket_2.addShoppingItem(box_of_chocolates);
         shoppingBasket_2.addShoppingItem(bottle_of_perfume);
         
+        System.out.println("===============================================================================");
+        System.out.println("Qtn" + "  Name" + "                              Price");
+        System.out.println("===============================================================================");
+        System.out.println(box_of_chocolates.getQtn() + "   " + box_of_chocolates.getName() + "         $" + box_of_chocolates.getPrice());
+        System.out.println(bottle_of_perfume.getQtn() + "   " + bottle_of_perfume.getName() + "         $" + bottle_of_perfume.getPrice());
+        System.out.println("----------------------------------------------------");
         System.out.println("Subtotal: $" + shoppingBasket_2.getSubTotal());
         System.out.println("Sales tax: $" + shoppingBasket_2.getSalesTax());
         System.out.println("Total: $" + shoppingBasket_2.getTotal());
-        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("===============================================================================");
 // --------------------------------------------------------------------------
-        System.out.println("Customer Shopping Receipt 2");
+        System.out.println();
+        System.out.println("CUSTOMER SHOPPING RECEIPT 3");
         ShoppingBasket shoppingBasket_3 = new ShoppingBasket();
         
         Item bottle_of_perfume_1 = new Item("imported bottle of perfume", "perfume", "imported", new BigDecimal("1"), new BigDecimal("27.99"));
@@ -41,11 +55,19 @@ public class PurchaseReceipt {
         Item packet_of_headache_pills = new Item("packet of headache pills", "medical", "domestic", new BigDecimal("1"), new BigDecimal("9.75"));
         Item box_of_imported_chocolates = new Item("box of imported chocolates", "food", "imported", new BigDecimal("1"), new BigDecimal("11.25"));
            
-        
+
         shoppingBasket_3.addShoppingItem(bottle_of_perfume_1);
         shoppingBasket_3.addShoppingItem(bottle_of_perfume_2);
         shoppingBasket_3.addShoppingItem(packet_of_headache_pills);
         shoppingBasket_3.addShoppingItem(box_of_imported_chocolates);
+        
+        System.out.println("===============================================================================");
+        System.out.println("Qtn" + "  Name" + "                              Price");
+        System.out.println("===============================================================================");
+        System.out.println(bottle_of_perfume_1.getQtn() + "   " + bottle_of_perfume_1.getName() + "         $" + bottle_of_perfume_1.getPrice());
+        System.out.println(bottle_of_perfume_2.getQtn() + "   " + bottle_of_perfume_2.getName() + "                  $" + bottle_of_perfume_2.getPrice());
+        System.out.println(packet_of_headache_pills.getQtn() + "   " + packet_of_headache_pills.getName() + "           $" + packet_of_headache_pills.getPrice());
+        System.out.println(box_of_imported_chocolates.getQtn() + "   " + box_of_imported_chocolates.getName() + "         $" + box_of_imported_chocolates.getPrice());
 
         System.out.println("Subtotal: $" + shoppingBasket_3.getSubTotal());
         System.out.println("Sales tax: $" + shoppingBasket_3.getSalesTax());
